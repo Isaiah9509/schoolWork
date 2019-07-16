4a82b272d4a156533c4b0040c80c4d46fece6ccf
Agentkush <52937433+Agentkush@users.noreply.github.com>
2019-07-16 11:22:03 -0400

    Update FinalProject.c

--- FinalProject.c
+++ FinalProject.c
@@ -13,15 +13,16 @@
 #include <stdlib.h> 
  
  //Structs
+typedef struct Person Person;
 struct Person {
 	char firstName[50];
 	char lastName[50];
 	char streetAddress[60];
 	char cityName[30];
 	char stateName[40];
-	char zipCode[40];
-	char phoneNumber[40];
-	char emailAdddress[60];
+	char zipCode[10];
+	char phoneNumber[12];
+	char emailAddress[60];
 };
  
 //Prototypes 
@@ -36,19 +37,17 @@ void viewAllContacts();
 /* 
  * Name:           main() 
  * Parameters:     None. 
- * Processes:       
- * Return Value:   An integer representing an error code; if the program ends without       
- *                 error, zero will be returned to the calling program or operating *      
- *                 system. 
+ * Processes:      Hosts a functional contact book where the user my Create, Retrieve, Update, or Delete contacts.
+ * Return Value:   None.                  
  */ 
  
 main() { 
     // Variables
     int exit = 0;
     // Temporary File
-    FILE* tmpfile(void);
+
     // Struct Variable
-    Person person;
+    Person contact;
 	
 	// Menu Loop
 	while (exit == 0) {
@@ -65,7 +64,7 @@ main() {
     switch (option) 
     { 
     case 1: 
-        addContact(&person); 
+        addContact(&contact); 
         break; 
     case 2: 
         searchContact(); 
@@ -74,7 +73,7 @@ main() {
         deleteContact(); 
         break; 
     case 4: 
-        viewAllContacts(); 
+        viewAllContacts(contact); 
         break; 
     case 5: 
         exit = 1;
@@ -95,9 +94,23 @@ main() {
  *                 error, zero will be returned to the calling program or operating *      
  *                 system. 
  */ 
+
+/* 
+ * Name:           addContact() 
+ * Parameters:     *person Passing the struct variable through the function.
+ * Processes:      Creates and stores the Name, Address, Phone Number, and Email Address of the contact.
+ * Return Value:   None.   
+ */ 
 void addContact(Person *person) {
+	// Local Variables
+	FILE *pb;  
+    pb = fopen("contacts.txt", "a+"); 
+
 	system("cls");
-	printf("Please enter the First Name of the contact: ");
+
+    printf("-----Add Contact-----"); 
+
+	printf("\nPlease enter the First Name of the contact: ");
 	scanf("%s", &person->firstName);
 	printf("\nPlease enter the Last Name of the contact: ");
 	scanf("%s", &person->lastName);
@@ -105,9 +118,19 @@ void addContact(Person *person) {
 	scanf(" %[^\n]", &person->streetAddress);
 	printf("\nPlease enter the City Name of the contact: ");
 	scanf("%s", &person->cityName);
-	printf("
+	printf("Please enter the State Name of the contact: ");
+	scanf("%s", &person->stateName);
+	printf("Please enter the  ZIP Code of the contact: ");
+	scanf("%s", &person->zipCode);
+	printf("Please enter the phone number of the contact: ");
+	scanf("%s", &person->phoneNumber);
+	printf("Please enter the email address of the contact: ");
+	scanf("%s", &person->emailAddress);
 	
-	printf("%s", &person->streetAddress);
+    fprintf(pb, "%s %s %s %s %s %s %s %s", person->firstName, person->lastName, person->streetAddress, person->cityName, person->stateName, person->zipCode, person->phoneNumber, person->emailAddress);
+    
+    fclose(pb);
+	
 	system("pause");
 
 }
@@ -120,8 +143,31 @@ void deleteContact() {
 	
 }
 
-void viewAllContacts() {
-	
+/* 
+ * Name:           viewAllContacts() 
+ * Parameters:     *person Passes the struct variable through the function.
+ * Processes:       List all contacts that are contained within the contact book.
+ * Return Value:   None.   
+ */ 
+void viewAllContacts(Person person) {
+    //Local Variables 
+    FILE *pb;     
+    pb = fopen("contacts.txt", "r"); 
+    system("cls"); 
+    printf("-----All Contacts-----"); 
+    while (fscanf(pb, "%s %s %s %s %s %s %s %s", person.firstName, person.lastName, person.streetAddress, person.cityName, person.stateName, person.zipCode, person.phoneNumber, person.emailAddress) != EOF) { 
+        printf("\n Name    :  %s %s", person.firstName, person.lastName); 
+        printf("\n Street  :  %s", person.streetAddress); 
+        printf("\n City    :  %s", person.cityName);
+        printf("\n State   :  %s", person.stateName);
+        printf("\n ZipCode :  %s", person.zipCode);
+        printf("\n Phone # :  %s", person.phoneNumber);
+        printf("\n Email @ :  %s", person.emailAddress);
+    } 
+
+    fclose(pb); 
+
+    system("pause");
 }
 
  
